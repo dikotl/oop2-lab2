503231f1216d56275ec03aac39911b11ae7cd9b1
dikotl <188217911+dikotl@users.noreply.github.com>
2026-03-17 01:22:50 +0200

    Implement undo\redo, add a new column for functions

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media.Animation;
 
 namespace Calculator;
 
@@ -12,7 +13,11 @@ internal record HistoryEntry(string Input, string Output);
 public partial class MainWindow : Window
 {
     private Evaluator _evaluator = new();
-    private Stack<HistoryEntry> _history = [];
+    private Stack<HistoryEntry> _undoStack = [];
+    private Stack<HistoryEntry> _redoStack = [];
+    private bool _isScientificVisible = false;
+
+    private HistoryEntry CurrentEntry => new(input.Text, output.Text);
 
     public MainWindow()
     {
@@ -22,6 +27,10 @@ public partial class MainWindow : Window
 
     private void InputButton_Click(object sender, RoutedEventArgs e)
     {
+        // New input breaks the redo chain.
+        _undoStack.Push(CurrentEntry);
+        _redoStack.Clear();
+
         input.Text += ((Button)sender).Content;
     }
 
@@ -30,11 +39,10 @@ public partial class MainWindow : Window
         try
         {
             output.Text = $"{_evaluator.Eval(input.Text):g}";
-            _history.Push(new HistoryEntry(input.Text, output.Text));
         }
         catch (ParseException ex)
         {
-            output.Text = $"Error! {ex.Message}";
+            output.Text = $"{ex.Message}";
             return;
         }
         catch (ExecutionException ex)
@@ -57,8 +65,11 @@ public partial class MainWindow : Window
 
     private void UndoButton_Click(object sender, RoutedEventArgs e)
     {
-        if (_history.TryPop(out HistoryEntry? entry))
+        if (_undoStack.TryPop(out HistoryEntry? entry))
         {
+            // Store the current state so we can redo.
+            _redoStack.Push(CurrentEntry);
+
             // Restore previous entry.
             input.Text = entry.Input;
             output.Text = entry.Output;
@@ -70,6 +81,20 @@ public partial class MainWindow : Window
         }
     }
 
+    private void RedoButton_Click(object sender, RoutedEventArgs e)
+    {
+        if (_redoStack.TryPop(out HistoryEntry? entry))
+        {
+
+            // Save the current state to undo before jumping forward.
+            _undoStack.Push(CurrentEntry);
+
+            // Restore the entry.
+            input.Text = entry.Input;
+            output.Text = entry.Output;
+        }
+    }
+
     private void EraseButton_Click(object sender, RoutedEventArgs e)
     {
         if (input.Text.Length > 0)
@@ -78,12 +103,46 @@ public partial class MainWindow : Window
         }
     }
 
-    private void input_KeyDown(object sender, KeyEventArgs e)
+    private void Input_KeyDown(object sender, KeyEventArgs e)
     {
         if (e.Key == Key.Enter)
         {
             e.Handled = true;
             EvalButton_Click(this, new());
         }
+    }
+
+    private void ToggleScientific_Click(object sender, RoutedEventArgs e)
+    {
+        // TODO: fix window resizing may break the animation start.
+
+        var widthAnimation = new DoubleAnimation
+        {
+            Duration = TimeSpan.FromSeconds(0.3),
+            EasingFunction = new ExponentialEase(),
+        };
+
+        if (!_isScientificVisible)
+        {
+            // Show the column.
+            ScientificColumn.Width = new GridLength(1, GridUnitType.Star);
+            widthAnimation.To = ActualWidth + 100;
+            Width += 100;
+        }
+        else
+        {
+            // Hide the column.
+            ScientificColumn.Width = new GridLength(0);
+            widthAnimation.To = ActualWidth - 100;
+            Width -= 100;
+            // We use a completed event to set column width to 0 only after
+            // the window has finished shrinking.
+            // widthAnimation.Completed += (s, ev) =>
+            // {
+            //     ScientificColumn.Width = new GridLength(0);
+            // };
+        }
+        _isScientificVisible = !_isScientificVisible;
+        BeginAnimation(WidthProperty, widthAnimation);
     }
 }
